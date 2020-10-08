e181065578cc7fd645120c38233298446f8ea636
Cyril <cyril.goldenschue@cpnv.ch>
2020-10-08 08:47:18 +0200

    Add query

--- modele.php
+++ modele.php
@@ -13,4 +13,15 @@ function getBD()
     return $connection;
 }
 
+function getClientAndPrime(){
+    $connection = getBD();
+
+    $req = "SELECT customers.name, firstname, rest, franchise, reduction, assurances.name, price, (price - reduction) as Prime FROM customers INNER JOIN customers_has_insurancemodels ON customers_has_insurancemodels.Customers_id = customers.id INNER JOIN primes ON primes.id = customers_has_insurancemodels.Franchises_id INNER JOIN assurances ON assurances.id = customers_has_insurancemodels.Assurances_id";
+
+    $result = $connection->query($req);
+
+    return $result;
+
+}
+
 
