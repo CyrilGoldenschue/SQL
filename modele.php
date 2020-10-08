67992b60b4e3bc5f9fcfbcdd580cd6b8d74c19c9
Cyril <cyril.goldenschue@cpnv.ch>
2020-10-08 08:24:08 +0200

    tout

---
+++ modele.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * @Description Connection to the database
+ * @return PDO
+ */
+function getBD()
+{
+    // connexion au serveur MySQL et à la BD
+    $connection = new PDO('mysql:host=localhost; dbname=assurancemaladie','ConnectDB', 'Adm1n123');
+    // permet d'avoir plus de détails sur les erreurs retournées
+    $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    return $connection;
+}
+
+
